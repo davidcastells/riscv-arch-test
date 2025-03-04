8f7eaa1f7ad3d9d2351eb77313cdcabf8860e498
David Castells-Rufas <david.castells@uab.cat>
2025-03-04 08:57:35 +0100

    correct dir

--- riscof-plugins/rv64/punxa_simple/riscof_punxa_simple.py
+++ riscof-plugins/rv64/punxa_simple/riscof_punxa_simple.py
@@ -160,7 +160,7 @@ class punxa_simple(pluginTemplate):
             # set up the simulation command. Template is for spike. Please change.
             # simcmd = self.dut_exe + ' -m8796093022208 --isa={0} +signature={1} +signature-granularity=8 {2}'.format(self.isa, sig_file, elf)
             # simcmd = simcmd + ';' + self.dut_exe + ' -m8796093022208 --isa={0} --log-commits -l my.elf 2> {1}'.format(self.isa, log_file)
-            simcmd = 'python  /home/dcr/INT_RISCV/punxa/test/risc-arch-test/tb_signature.py  +signature={} +signature-granularity=8 {}'.format( sig_file, elf)
+            simcmd = 'python  /home/dcr/INT_RISCV/punxa/test/riscv-arch-test/tb_signature.py  +signature={} +signature-granularity=8 {}'.format( sig_file, elf)
           else:
             simcmd = 'echo "NO RUN"'
 
